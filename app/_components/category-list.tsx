eca63e1bdedfdfe6d0ec73eb0ac690f9c5c77d86
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-05-03 23:38:12 -0300

    style: Reajuste no componente

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,7 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="flex w-full items-center gap-3 overflow-x-auto px-4 [&::-webkit-scrollbar]:hidden">
+    <div className="flex w-full items-center gap-3 overflow-x-scroll px-4 [&::-webkit-scrollbar]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
