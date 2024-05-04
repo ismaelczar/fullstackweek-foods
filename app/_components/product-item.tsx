17b5432a7dbc3f32a9b3dd735afc6384fbbfdcf2
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-05-04 15:08:05 -0300

    refactor: Ajuste no componente ProductItem

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -2,6 +2,7 @@ import { Prisma, Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import { calculateTotalPrice, formatCurrency } from "../_helpers/price";
+import { ArrowDown } from "lucide-react";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -26,20 +27,28 @@ const ProductItem = ({ product }: ProductItemProps) => {
             fill
             className="rounded-lg object-cover shadow-md"
           />
+          <span className="absolute left-2 top-2 z-50 flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-xs font-semibold text-white">
+            <ArrowDown size={12} />
+            {product.discountPercentage}%
+          </span>
         </div>
       </div>
 
       <div className="flex flex-col">
-        <span className="truncate">{product.name}</span>
+        <span className="truncate pt-2 text-sm font-medium">
+          {product.name}
+        </span>
         <div className="flex items-center gap-2">
           <strong>{formatCurrency(calculateTotalPrice(product))}</strong>
           {product.discountPercentage > 0 && (
-            <span className="text-xs line-through">
+            <span className="text-xs text-[#7E8392] line-through">
               {formatCurrency(Number(product.price))}
             </span>
           )}
         </div>
-        <span className="truncate">{product.restaurant.name}</span>
+        <span className="truncate text-xs text-[#7E8392]">
+          {product.restaurant.name}
+        </span>
       </div>
     </Link>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 
-import PromoBanner from "@/public/PromoBanner.png";
+import PromoBaner01 from "@/public/promo-banner-01.png";
+import PromoBaner02 from "@/public/promo-banner-02.png";
+
 import { Button } from "./_components/ui/button";
 import Link from "next/link";
 import { ChevronRight } from "lucide-react";
 import ProductList from "./_components/product-list";
+import PromoBanner from "./_components/promo-banner";
 
 export default function Home() {
   return (
@@ -17,19 +19,12 @@ export default function Home() {
         <Search />
       </div>
 
-      <div className="py-6">
+      <div className="pt-6">
         <CategoryList />
       </div>
 
-      <div>
-        <Image
-          src={PromoBanner}
-          alt="Banner que destaca percentual de desconto nas pizzas"
-          width={0}
-          height={0}
-          className="h-auto w-full object-contain"
-          quality={100}
-        />
+      <div className="pt-6">
+        <PromoBanner src={PromoBaner01} alt={""} />
       </div>
 
       <div className="flex items-center justify-between px-5 pt-6">
@@ -46,8 +41,28 @@ export default function Home() {
       </div>
 
       <div className="space-y-4 py-6">
+        {/* TODO:Ao clicar no produto, direcionar para o componente /product/[id].tsx */}
         <ProductList />
       </div>
+
+      <div className="px-5">
+        <PromoBanner src={PromoBaner02} alt={""} />
+      </div>
+
+      <div className="flex items-center justify-between px-5 pt-6">
+        <h2 className="font-semibold">Restaurantes Recomendados</h2>
+        <Button
+          asChild
+          className="border-node bg-transparent p-0"
+          variant={"link"}
+        >
+          <Link href={""} className="flex items-center">
+            Ver todos <ChevronRight size={16} />
+          </Link>
+        </Button>
+      </div>
+
+      {/* TODO:Ao clicar no restaurante, direcionar para o componente /restaurante/[id].tsx */}
     </>
   );
 }
