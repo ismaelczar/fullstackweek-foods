093ec2024cc3cb46598fed3ff7246688874eb828
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-05-04 23:47:25 -0300

    refactor: Aplicando melhorias

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -7,6 +7,7 @@ import { Badge } from "./ui/badge";
 import bikeIco from "@/public/bikeIco.png";
 import timeIco from "@/public/timeIco.png";
 import { formatCurrency } from "../_helpers/price";
+import { Button } from "./ui/button";
 
 interface RestauranteItemProps {
   restaurant: Restaurant;
@@ -14,7 +15,7 @@ interface RestauranteItemProps {
 
 const RestauranteItem = ({ restaurant }: RestauranteItemProps) => {
   return (
-    <div className="h-[190px] w-[266px] min-w-[266px]">
+    <div className="min-w-[266px] max-w-[266px]">
       <div className="w-full space-y-2">
         <div className="relative aspect-square h-[136px] w-full object-cover ">
           <Link href={""}>
@@ -22,7 +23,7 @@ const RestauranteItem = ({ restaurant }: RestauranteItemProps) => {
               src={restaurant.imageUrl}
               alt={restaurant.name}
               fill
-              className="rounded-md"
+              className="ob rounded-md object-cover"
             />
           </Link>
           <Badge className="absolute left-2 top-4 z-50 flex items-center gap-[2px] bg-white text-black">
@@ -34,7 +35,12 @@ const RestauranteItem = ({ restaurant }: RestauranteItemProps) => {
             5.0
           </Badge>
 
-          <HeartIcon className="absolute right-2 top-3 z-50 rounded-full bg-slate-500 fill-white p-1 text-base text-white" />
+          <Button
+            size="icon"
+            className="absolute right-2 top-2 rounded-full bg-slate-500"
+          >
+            <HeartIcon size={16} className=" fill-white text-white" />
+          </Button>
         </div>
 
         <div>
@@ -43,13 +49,11 @@ const RestauranteItem = ({ restaurant }: RestauranteItemProps) => {
             <div className="flex items-center gap-1">
               <Image src={bikeIco} alt="" />
 
-              {Number(Number(restaurant.deliveryFee)) === 0 ? (
-                <span className="text-xs text-gray-400">Entrega Grátis</span>
-              ) : (
-                <span className="text-xs text-gray-400">
-                  {formatCurrency(Number(restaurant.deliveryFee))}
-                </span>
-              )}
+              <span className="text-xs text-gray-400">
+                {Number(restaurant.deliveryFee) === 0
+                  ? "Entrega Grátis"
+                  : formatCurrency(Number(restaurant.deliveryFee))}
+              </span>
             </div>
 
             <div className="flex items-center gap-1">
